04da048efb70a0510b28e2b184f09db9d56e52ea
RaymondLoranger <Raymond.Loranger@bell.net>
2023-05-15 08:19:58 -0400

    0.1.28

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Buzzword.Bingo.Square.MixProject do
   def project do
     [
       app: :buzzword_bingo_square,
-      version: "0.1.27",
+      version: "0.1.28",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       name: "Buzzword Bingo Square",
